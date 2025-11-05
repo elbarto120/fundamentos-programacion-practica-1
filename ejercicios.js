6f561ba08986f2515923eb82c80d2da8ea612556
Fernando-Castro-Hernandez <jesus.castro@tecdesoftware.edu.mx>
2025-11-05 11:43:10 -0600

    Ejercicio 2.3 resuelto

--- ejercicios.js
+++ ejercicios.js
@@ -111,7 +111,13 @@ function evaluarNota(nota) {
  */
 function mayorDeTres(a, b, c) {
   // TODO: Encuentra y retorna el mayor de los tres nÃºmeros
-  return 0;
+  if (a >= b & a >= c) {
+    return a    
+  } else if (b >= a & b >= c) {
+    return b
+  } else{
+    return c
+  }
 }
 
 /**
